86b59028fe2d45b7e47b8381c31eaf61c802e866
erobin27 <elijah1998robinson@gmail.com>
2021-12-10 16:52:53 -0500

    add .csv files to folder

--- ZillowDashboard.py
+++ ZillowDashboard.py
@@ -1,5 +1,5 @@
 import pandas as pd;
 
-url = 'https://raw.githubusercontent.com/erobin27/ZillowDashboard/master/ListAndSalePrice.csv'
+url = 'https://raw.githubusercontent.com/erobin27/ZillowDashboard/master/rawdata/ListAndSalePrice.csv'
 df = pd.read_csv(url,index_col=0)
 print(df.head())
